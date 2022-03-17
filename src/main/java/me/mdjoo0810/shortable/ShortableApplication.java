a65323836b4954b8ac61bddfb0184592c2eaf64a
mdjoo0810 <mdjoo0810@gmail.com>
2022-03-17 11:17:09 +0900

    first commit

---
+++ src/main/java/me/mdjoo0810/shortable/ShortableApplication.java
@@ -0,0 +1,13 @@
+package me.mdjoo0810.shortable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortableApplication.class, args);
+    }
+
+}
