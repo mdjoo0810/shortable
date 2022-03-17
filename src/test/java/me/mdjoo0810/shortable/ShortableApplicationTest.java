ce44696a2b148cb598731539429cd0a0eb648c4b
mdjoo0810 <mdjoo0810@gmail.com>
2022-03-17 13:08:56 +0900

    main class

---
+++ src/test/java/me/mdjoo0810/shortable/ShortableApplicationTest.java
@@ -0,0 +1,14 @@
+package me.mdjoo0810.shortable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShortableApplicationTest {
+
+//    @Test
+//    void main() {
+//        ShortableApplication.main(new String[] {});
+//    }
+
+}
