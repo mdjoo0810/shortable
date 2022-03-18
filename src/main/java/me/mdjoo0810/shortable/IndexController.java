ff38d3535f6093f032be1a79cfb29d176c0f8c9d
mdjoo0810 <mdjoo0810@gmail.com>
2022-03-18 16:35:46 +0900

    feat: index page

--- src/main/java/me/mdjoo0810/shortable/IndexController.java
+++ src/main/java/me/mdjoo0810/shortable/IndexController.java
@@ -1,15 +1,36 @@
 package me.mdjoo0810.shortable;
 
 import lombok.RequiredArgsConstructor;
+import me.mdjoo0810.shortable.common.enums.CookieKey;
+import me.mdjoo0810.shortable.member.application.MemberFacade;
+import me.mdjoo0810.shortable.member.domain.entity.MemberInfo;
+import me.mdjoo0810.shortable.utils.CookieUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
+    private final MemberFacade memberFacade;
+
     @GetMapping
-    public String index() {
+    public String index(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        Optional<Cookie> loginUser = CookieUtils.getCookie(request, "LOGIN_USER");
+
+        if (loginUser.isEmpty()) {
+            MemberInfo memberInfo = memberFacade.registerAnonymous();
+            CookieUtils.addCookie(response, CookieKey.LOGIN_USER.getValue(), memberInfo.getEmail(), 60 * 60 * 24 * 7);
+        }
+
         return "index";
     }
 
--- src/test/java/me/mdjoo0810/shortable/url/web/IndexControllerTest.java
+++ src/test/java/me/mdjoo0810/shortable/url/web/IndexControllerTest.java
@@ -1,16 +1,52 @@
 package me.mdjoo0810.shortable.url.web;
 
 import me.mdjoo0810.shortable.IndexController;
+import me.mdjoo0810.shortable.common.enums.CookieKey;
+import me.mdjoo0810.shortable.member.application.MemberFacade;
+import me.mdjoo0810.shortable.member.domain.entity.MemberInfo;
+import me.mdjoo0810.shortable.member.domain.entity.MemberInfoFixture;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.http.Cookie;
+
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class IndexControllerTest {
 
+    MemberFacade memberFacade = mock(MemberFacade.class);
+
     @Test
-    void index() {
-        IndexController controller = new IndexController();
-        String index = controller.index();
+    void index_not_have_cookie() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        when(memberFacade.registerAnonymous()).thenReturn(MemberInfoFixture.get());
+
+        IndexController controller = new IndexController(memberFacade);
+        controller.index(request, response);
+        Cookie cookie = response.getCookie(CookieKey.LOGIN_USER.getValue());
+        assertEquals(MemberInfoFixture.get().getEmail(), Objects.requireNonNull(cookie).getValue());
+    }
+
+    @Test
+    void index_has_cookie() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        MemberInfo memberInfo = MemberInfoFixture.get();
+
+        Cookie tmpCookie = new Cookie(CookieKey.LOGIN_USER.getValue(), memberInfo.getEmail());
+        request.setCookies(tmpCookie);
+
+
+        IndexController controller = new IndexController(memberFacade);
+        String index = controller.index(request, response);
+
         assertEquals("index", index);
     }
 
